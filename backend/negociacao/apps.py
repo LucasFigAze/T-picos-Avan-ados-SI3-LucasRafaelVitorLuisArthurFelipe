328fb0da56e5c5ae71dceebceeb0027e0e9fabcd
Felipe Santos <felipessantos2004@gmail.com>
2025-11-09 14:19:27 -0300

    feat: create initial backend structure

--- backend/negociacao/apps.py
+++ backend/negociacao/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GestaoConfig(AppConfig):
+class NegocicaoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestao'
+    name = 'negociacao'
