a2d63107ab4db0187fe961d004a862a27176895f
Luis Moreira <luisf.moreira324@gmail.com>
2025-12-04 22:58:47 -0300

    [feat]: finishing integration with discord bot

--- backend/negociacao/management/commands/run_discord_bot.py
+++ backend/negociacao/management/commands/run_discord_bot.py
@@ -6,109 +6,104 @@ from datetime import datetime
 from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from asgiref.sync import sync_to_async
-
-from negociacao.services import chamar_gemini
+from negociacao.services import processar_mensagem_ia, buscar_clientes_para_outbound, confirmar_envio_outbound
 
 load_dotenv()
 
-ANSWER_TIME=12
-TIMEZONE= 'America/Sao_Paulo'
-MINI_DELAY=8 # Deu para perceber no discord aqui
-BOT_ID=1436381275312230575
- 
-class MyClient(discord.Client):    
+ANSWER_TIME = 12
+TIMEZONE = 'America/Sao_Paulo'
+MINI_DELAY = 8
+BOT_ID = 1436381275312230575
+
+
+class MyClient(discord.Client):
     def __init__(self, *, intents, **options):
         self.messages = {}
-        self.typing   = {}
+        self.typing = {}        
         super().__init__(intents=intents, **options)
 
-    def scheduleStartConversation(self, user_id, installments):        
-        if self.is_ready():            
-            self.loop.create_task(self.startConversation(user_id, installments))
-            return True
-        return False
-
-    async def startConversation(self, user_id, installments):
-        user = await self.fetch_user(user_id)
+    async def startConversation(self, discord_id, content, user_id, initial_value=None ):
+        user = await self.fetch_user(discord_id)
         if user:
-            print(user)
-            await user.send(installments)
+            print(f"Enviando mensagem para {user}")
+            await user.send(content)
+            if initial_value is not None:
+                await confirmar_envio_async(user_id, initial_value, content)
         else:
-            print("deu ruim aqui")   
+            print("Não conseguiu localizar o usuário.")
+
+    async def running_bot(self):
+        while True:            
+            clientes = await buscar_clientes_async()
+            print(clientes)
+            if clientes:
+                for cliente in clientes:
+                    self.loop.create_task(
+                        self.startConversation(cliente['discord_id'], cliente['mensagem_texto'], cliente['id'], cliente['valor_divida'])
+                    )
+            await asyncio.sleep(60)
 
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        await asyncio.sleep(2)
-        self.loop.create_task(self.startConversation('275275075345973250', "Ola, isso é um teste por enquanto"))        
-
-    # async def on_typing(self, channel, user, when):
-    #     print(f"{user} está digitando em {channel} às {when}")
+        # Teste inicial (opcional)
+        # await self.startConversation('275275075345973250', "Olá, isso é um teste por enquanto")
+        self.loop.create_task(self.running_bot())
 
     async def on_raw_typing(self, payload):
-        if payload.guild_id is not None:
-            print("Mensagem de servidor aqui")
-        else:
-            print(f"adicionou aqui: {payload.timestamp}")
+        if payload.guild_id is None:
             self.typing[payload.user_id] = payload.timestamp
-            # print(payload.timestamp)
-            # if payload.user_id not in self.typing.keys():
-            #     self.typing[payload.user_id] = [payload.timestamp]
-            # else:
-            #     self.typing[payload.user_id].append(payload.timestamp)
 
-    async def on_message(self, message):        
-        content_struct = {
-                "content": message.content, 
-                "time": message.created_at
-            }                
-        if message.author.id == BOT_ID: 
+    async def on_message(self, message):
+        content_struct = {"content": message.content, "time": message.created_at}
+
+        if message.author.id == BOT_ID:
             return
-        
-        if message.author.id not in self.messages.keys():
-            print("primeira mensagem enviada")            
-            self.messages[message.author.id] = [content_struct]                                    
-            await asyncio.sleep(ANSWER_TIME)
-            while True:
-                print(self.typing)
-                print(message.author.id)                                
-                if message.author.id in self.typing:                    
-                    diff_time =  datetime.now(pytz.timezone(TIMEZONE)) - self.typing[message.author.id].astimezone(pytz.timezone(TIMEZONE))             
-                    print(diff_time)
-                    if diff_time.seconds > MINI_DELAY:
-                        break                    
-                    await asyncio.sleep(MINI_DELAY)
-                    
 
-            content = ""
-            for text in self.messages[message.author.id]:
-                content += text["content"] + " "
-            
-            resposta = await processar_mensagem_async(
-                            str(message.author.id), 
-                            content
-            )
+        if message.author.id not in self.messages:
+            self.messages[message.author.id] = [content_struct]
+            await asyncio.sleep(ANSWER_TIME)
+
+            while True:
+                if message.author.id in self.typing:
+                    diff_time = datetime.now(pytz.timezone(TIMEZONE)) - self.typing[message.author.id].astimezone(
+                        pytz.timezone(TIMEZONE)
+                    )
+                    if diff_time.seconds > MINI_DELAY:
+                        break
+                    await asyncio.sleep(MINI_DELAY)
+                
+                if message.author.id in self.messages:
+                    break     
+
+            content = " ".join([text["content"] for text in self.messages[message.author.id]])
+            print(f"Teste conteúdo: {content} ")
+            resposta = await processar_mensagem_async(str(message.author.id), content)
 
             if resposta:
                 await message.channel.send(resposta)
-                
+
             del self.messages[message.author.id]
-            del content
-                                    
+
         else:
-            print(f"{len(self.messages[message.author.id])}° mensagem enviada")
-            diff_time = content_struct["time"] - self.messages[message.author.id][0]["time"] 
-            print(diff_time.seconds)
-            # print((content_struct["time"] - self.messages[message.author.id][0]["time"]))
-            self.messages[message.author.id].append(content_struct)        
-    
+            self.messages[message.author.id].append(content_struct)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.typing = True
 intents.dm_typing = True
 intents.dm_messages = True
-processar_mensagem_async = sync_to_async(chamar_gemini)
+processar_mensagem_async = sync_to_async(processar_mensagem_ia)
+buscar_clientes_async = sync_to_async(buscar_clientes_para_outbound)
+confirmar_envio_async = sync_to_async(confirmar_envio_outbound)
 
+class Command(BaseCommand):
+    help = "Roda o bot do Discord"
 
-client = MyClient(intents=intents)
-client.run(os.getenv('DISCORD_TOKEN'))
+    def handle(self, *args, **options):
+        token = os.getenv("DISCORD_TOKEN")
+        if not token:
+            raise Exception("Variável de ambiente DISCORD_TOKEN não encontrada!")
+
+        client = MyClient(intents=intents)
+        client.run(token)
